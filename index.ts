1b83b60218efcebe95c8e10787ada10584c4457c
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-10-18 22:33:10 +0300

    terminat c04

---
+++ index.ts
@@ -0,0 +1,11 @@
+// declararea variabilelor
+
+const myName: string = 'Bogdan'; // variabila constanta de tip string
+
+// typescript este un decorator pentru javascript
+
+// daca compilam ceva de genul "const myName: string = 29" chiar daca
+// nu se va compila din punct de vedere typescript, codul este cod
+// valid de js
+
+// tot codul de js este cod valid de ts
