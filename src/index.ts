696661798a129f55e2b20d36b84e73fd64bbb935
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-10-19 12:16:17 +0300

    debugging + tipul unknown si never

--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,35 @@ const tuple: [string, number, boolean] = ['Bogdan', 10, true];
 // * TIPUL ANY
 let t: any = [1, 'test', false]; // un array cu diverse tipuri
 t = 1; // acum are doar valoarea 1
+
+// * TIPUL UNKNOWN
+let whatsThis: unknown = []; // unknown poate fi atribuit doar unei variabile de tipul any
+// u = 12; // unknown poate lua absolut orice valoare, similar cu any
+let myNumber: number;
+// myNumber = whatsThis; // eroare, unknown nu este atribuibil catre tipul number
+
+// * TIPUL NEVER
+let notGoingToHappen: never; // tipului never nu i se poate atribui niciun fel de alt tip (nici macar any)
+// nu este menit pentru variabile, menit pentru tipul de return al unor functii
+function displayError(): never {
+  // typescript arata ca aceasta functie returneaza void, dar este defapt never (deoarece arunca o eroare si nu mai continua executia codului,
+  // cum ar fi fost daca era void)
+  // executia nu ajunge niciodata in afara functiei
+  throw new Error('Something bad happened');
+}
+
+function displayName(name: string) {
+  console.log(name);
+}
+
+displayName('Bogdan');
+// displayName(null); // strict null checks
+
+// * DEBUGGING
+// typescript nu poate fi rulat nici in node.js, nici in browser (in niciunul din mediile care ruleaza nativ javascript),
+// poate fi rulat doar de catre compilatorul de typescript
+// --> run & debug in vscode --> create launch.json file cu node.js ca template
+// iar din tsconfig.json setam sourceMap: true (deja setat in versiunile mai noi)
+// fisierul .map mapeaza fisierul de javascript la fisierul de typescript
+// --> din launch.json la "program" schimbam din src/index.ts in dist/index.js
+// F5 --> start debugging
